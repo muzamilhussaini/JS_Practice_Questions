4b1e463de12bbdee391beb0d0f7becff33c6cefe
Muzamilhussaini <karbalaimuzammil50@gmail.com>
2026-08-27 13:57:49 -0700

    Renamed Variables folder to Day-01-Variables

---
+++ Day-01-Variables/script.js
@@ -0,0 +1,18 @@
+//(Challenge)
+// Write code that stores:
+// Your name
+// Your age
+// Your university
+// Then print:
+// My name is Muzamil.
+// I am 21 years old.
+// I study at BUITEMS.
+// Use template literals (backticks `).
+
+const fullName = "Muzamil";
+let age = 21;
+let university = "BUITEMS";
+
+console.log(`My name is ${fullName}.`);
+console.log(`I am ${age} years old.`);
+console.log(`I study at ${university}.`);
